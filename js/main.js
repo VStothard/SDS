82f7eb62b9c216848e4ba0de0d5c25df0b135e81
Verity Stothard <vjstothard@gmail.com>
2015-10-24 14:55:19 +1100

    Fixed issue with mobile drop menu not disappearing when window is expanded. Styled desktop portfolio drop down menu. Validated html and css.

--- js/main.js
+++ js/main.js
@@ -10,6 +10,7 @@ $(document).ready(function(){
 	var portDrop = $('#portfolio-drop'); //mobile poftfolio menu drop
 	var deskPortDropToggle = $('#desk-port-drop-toggle');
 	var deskPortDrop = $('#desk-portfolio-drop');
+	var navBar = $('#nav-cont');
 
 //HIDE MENU ON LOAD
 
@@ -17,8 +18,21 @@ $(document).ready(function(){
 	portDrop.hide();
 
 //MENU FUNCTIONS
-
+	//open/close menu when click hamburger icon
 	menuOpen.click(function(){
+		dropMenu.slideToggle('fast', 'linear');
+		portDrop.css("display", "none"); /*resets the portfolio drop menu when you click open or close. Stops it from staying open if the user doesn't close it*/
+
+	});
+	//close menu when click outside of NavBar/dropMenu
+	$(document).on('click', function(event) {
+	  if (!$(event.target).closest(navBar).length) {
+	    // Hide the menus.
+	    dropMenu.css("display", "none"); /*slideToggle('fast', 'linear');*/
+	  }
+	});
+	//close dropMenu when mouse leaves the area (fixes bug with dropMenu not hiding when user rescales the window)
+	dropMenu.mouseleave(function(){
 		dropMenu.slideToggle('fast', 'linear');
 	});
 
@@ -28,10 +42,28 @@ $(document).ready(function(){
 		portDrop.slideToggle('fast', 'linear');
 	});
 
-	/**When desktop user clicks or hovers 'portfoilio', opens drop down menu**/
+	/**When desktop user hovers over deskPortToggle, opens drop down menu**/
 	function mobDrop () {
 		deskPortDrop.slideToggle('fast', 'linear');
 	}
 
-	deskPortDropToggle.hover(mobDrop, mobDrop);
+	deskPortDropToggle.click(function(){
+		deskPortDrop.slideToggle('fast', 'linear');
+	});
+
+	$(document).on('click', function(event) {
+	  if (!$(event.target).closest(deskPortDropToggle).length) {
+	    // Hide the menus.
+	    deskPortDrop.css("display", "none"); /*slideToggle('fast', 'linear');*/
+	  }
+	});
+
+	/*
+	deskPortDropToggle.hover(mobDrop, function() {
+		//close menu when mouse leaves deskPortDrop
+		deskPortDrop.mouseleave(function(){
+			deskPortDrop.slideToggle('fast', 'linear');
+		});
+	});*/
+
 });
